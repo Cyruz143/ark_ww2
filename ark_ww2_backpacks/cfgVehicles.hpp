224cf587998b500861789000ba87365cf5f543c4
Cyruz143 <root.cyruz@gmail.com>
2019-05-16 15:23:06 +0100

    More backpack fixing

--- ark_ww2_backpacks/cfgVehicles.hpp
+++ ark_ww2_backpacks/cfgVehicles.hpp
@@ -4,6 +4,11 @@ class CfgVehicles {
     class B_LIB_AssaultPack_Base;
     class B_LIB_SOV_RA_MGAmmoBag;
     class B_LIB_SOV_RA_MedicalBag;
+    class B_LIB_US_Backpack_RocketBag;
+    class B_LIB_US_Radio;
+    class B_LIB_GER_Radio;
+    class B_LIB_GER_Panzer;
+    class B_LIB_GER_MedicBackpack;
 
     class fow_b_uk_p37: Bag_Base {
        maximumLoad = 320;
@@ -16,7 +21,7 @@ class CfgVehicles {
     class fow_b_uk_p37_radio: fow_b_uk_p37 {
        maximumLoad = 320;
     };
-  
+
     class fow_b_uk_piat: fow_b_uk_p37 {
        maximumLoad = 320;
     };
@@ -129,4 +134,103 @@ class CfgVehicles {
        maximumLoad = 320;
     };
 
+    class B_LIB_US_Bandoleer: B_LIB_AssaultPack_Base {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_Backpack: B_LIB_AssaultPack_Base {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_Backpack_Bandoleer: B_LIB_US_Backpack {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_Backpack_RocketBag_Empty: B_LIB_US_Backpack_RocketBag {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_Backpack_Mk2: B_LIB_US_Backpack {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_Radio_ACRE2: B_LIB_US_Radio {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_M36: B_LIB_AssaultPack_Base {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_M36_Bandoleer: B_LIB_US_M36 {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_M36_Rocketbag_Empty: B_LIB_US_M36 {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_M36_Rope: B_LIB_US_M36 {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_US_Radio_ACRE2: B_LIB_US_Radio {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_GER_A_frame: B_LIB_AssaultPack_Base {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_DAK_A_frame: B_LIB_GER_A_frame {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_GER_Backpack: B_LIB_AssaultPack_Base {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_GER_SapperBackpack_empty: B_LIB_GER_Backpack {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_GER_Radio_ACRE2: B_LIB_GER_Radio {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_GER_Panzer_Empty: B_LIB_GER_Panzer {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_GER_MedicBackpack_Empty: B_LIB_GER_MedicBackpack {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_GER_Tonister34_cowhide: B_LIB_GER_MedicBackpack_Empty {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_UK_HSack: B_LIB_AssaultPack_Base {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_UK_HSack_Cape: B_LIB_UK_HSack {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_UK_HSack_Tea: B_LIB_UK_HSack {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_UK_HSack_Blanco: B_LIB_UK_HSack {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_UK_HSack_Blanco_Cape: B_LIB_UK_HSack_Cape {
+       maximumLoad = 320;
+    };
+
+    class B_LIB_UK_HSack_Blanco_Tea: B_LIB_UK_HSack_Tea {
+       maximumLoad = 320;
+    };
 };
