49789ddfffb02714f938571b361d08a8c5d70250
Cyruz143 <root.cyruz@gmail.com>
2019-05-16 15:24:37 +0100

    Adding build tools

---
+++ addons/ark_ww2_vests/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+    class ark_ww2_vests
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.86;
+        requiredAddons[] = {
+            "ark_vests",
+            "fow_main",
+            "fow_characters",
+            "WW2_Core_c_IF_ZZZ_LastLoaded_c"
+        };
+        author = "ARK";
+        authorUrl = "http://www.ark-group.org";
+    };
+};
+
+#include "cfgWeapons.hpp"
